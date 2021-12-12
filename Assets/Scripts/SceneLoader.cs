1f4ab7641fdf8e3f3a6a83cb56464aec05e4a4a2
Takayama <823ab172@gmail.com>
2021-12-12 16:21:10 +0900

    AddTitleScenes
    タイトルシーン作成:作ったタイトルシーンにタイトル・スタートボタン・ランキングボタン仮置きしました

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System;
+
+public class SceneLoader : MonoBehaviour
+{
+    public enum Scenes
+    {
+        Title,
+        Runking,
+        Main
+    }
+    [SerializeField] Scenes sceneName;
+
+    //<summary>
+    //インスペクターで選ばれたシーンに飛ぶ
+    //</summary>
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(Enum.GetName(typeof(Scenes), sceneName));
+    }
+
+}
